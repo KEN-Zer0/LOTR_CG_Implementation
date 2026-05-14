51f70927e4a2e784515435db0227dd36785be1e5
KEN_ZER0 <94794855+KEN-Zer0@users.noreply.github.com>
2026-05-14 14:30:35 +0200

    Table structure, deck shuffling. Reduction of imports in congi/all_cards_deck.py. TODO: *Test, documentation *Enemies engagement logic, battle logic, defense logic *Phases methods and game logic, more test, validation of decision *Name change Land->Travel_Location

--- src/table/table.py
+++ src/table/table.py
@@ -1,13 +1,14 @@
 from src.cards import *
 from config import *
+import random
 
 class Table:
-    table_threat: int
+    table_threat: int = 0
 
     quest_deck: list[Quest]
     active_travel_location: Land
 
-    player_deck: list[Hero, Ally]
+    player_deck: list[Ally]
     player_heros: list[Hero]
     player_hand: list[Ally]
     player_engagement: list[Hero, Ally]
@@ -18,13 +19,18 @@ class Table:
 
     def __init__(self):
         # import from deck
-        self.quest_deck = []
-        self.player_deck = []
-        self.player_heros = []
+        self.quest_deck = all_cards_deck.quest_deck.copy()
+        self.player_deck = all_cards_deck.player_deck.copy()
+        self.player_heros = all_cards_deck.hero_pool.copy()
         self.encounter_deck = all_cards_deck.encounter_deck.copy()
 
         self.calculate_table_threat()
+        self.shuffle_deck()
 
     def calculate_table_threat(self):
         for hero in self.player_heros:
             self.table_threat += hero.threat
+
+    def shuffle_deck(self):
+        random.shuffle(self.player_deck)
+        random.shuffle(self.encounter_deck)
